952cd049fa65817e09328eeef38287a57825be57
chiemezie1 <chiemezieagbo1@gmail.com>
2024-07-31 21:18:33 +0100

    ./0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-simple flask app
+A simple flask app, that returns "Hello HBNB!"
 """
 from flask import Flask
 app = Flask(__name__)
@@ -8,4 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
-    print("Hello HBNB!")
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
