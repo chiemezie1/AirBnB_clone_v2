66f9eccc43d5ff9d86170263da136de07628ae42
chiemezie1 <chiemezieagbo1@gmail.com>
2024-08-01 02:41:33 +0100

    ./2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,9 +15,11 @@ def hello_route():
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
